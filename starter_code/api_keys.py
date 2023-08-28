8234aff1077ce93eecc43ce2e3ecca834b2320f8
ericfayhuynh <ericfayhuynh@gmail.com>
2023-08-27 18:42:14 -0700

    Update api_keys.py

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "a5dadc4e8b3c791d330aa09626d76678"
+weather_api_key = "removed"
 
 # Geoapify API Key
-geoapify_key = "5aff2a4d0efe4034a5ba909e54dbded8"
+geoapify_key = "removed"
